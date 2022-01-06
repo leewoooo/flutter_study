da7b6173c23ca9a2673a74ca81998a5a969b5f68
leewoooo <leecoding2285@gmail.com>
2022-01-07 00:21:45 +0900

    Add: Article Domain and Fetch

---
+++ instagram/lib/data/article_data.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:instagram/domain/article.dart';
+
+class ArticleData{
+  final String serverURL;
+
+  ArticleData({
+    required this.serverURL
+  });
+
+  // https://docs.flutter.dev/cookbook/networking/background-parsing
+  List<Article> parseArticle(String responseBody){
+    final List<dynamic> decoded = jsonDecode(responseBody);
+    return decoded.map((decodedMap) => Article.fromJSON(decodedMap)).toList();
+  }
+
+  Future<List<Article>> fetchArticles(http.Client client) async {
+    final response = await client.get(Uri.parse(serverURL));
+    return parseArticle(response.body);
+  }
+}
---
+++ instagram/lib/domain/article.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class Article {
+  final int id;
+  final Image image;
+  final int likes;
+  final String date;
+  final String content;
+  final bool liked;
+  final String user;
+
+  Article({
+    required this.id,
+    required this.image,
+    required this.likes,
+    required this.date,
+    required this.content,
+    required this.liked,
+    required this.user
+  });
+
+  factory Article.fromJSON(Map<String, dynamic> json){
+    Image image = Image.network(json['image'] as String);
+    return Article(
+        id: json['id'] as int,
+        image: image,
+        likes: json['likes'] as int,
+        date: json['date'] as String,
+        content: json['content'] as String,
+        liked: json['liked'] as bool,
+        user: json['user'] as String,
+    );
+  }
+
+  factory Article.fromImageFile(File file, String content, int id){
+    return Article(
+        id: id,
+        image: Image.file(file),
+        likes: 9,
+        date: 'mockData',
+        content: content,
+        liked: false,
+        user: 'foobar'
+    );
+  }
+}
+
+
